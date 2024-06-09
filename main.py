af8ce2d11eb97eb6688f10059bd0462c33fcab6b
vitalie stinca <vitalicstinca123@gmail.com>
2024-06-09 17:19:41 +0300

    feat: Implement Create operation

--- main.py
+++ main.py
@@ -7,6 +7,32 @@ from sqlalchemy.orm import sessionmaker
 # Database configuration
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocomit = False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+# SQLAlchemy models
+class Item(Base):
+    __tablenme__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+
+
+Base.metadata.create_all(bind=engine)
+
+# FastAPI app instance
+app = FastAPI()
+
+
+# CRUD operations
+# Create (Create)
+@app.post("/items/")
+async def create_item(name: str, description: str):
+    db = SessionLocal()
+    db_item = Item(name=name, description=description)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
 
