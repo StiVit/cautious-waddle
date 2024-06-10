dd5358ad21dff64f4d0b2db35d948159fe5ac240
vitalie stinca <vitalicstinca123@gmail.com>
2024-06-10 15:25:16 +0300

    feat: Move database configuration into a separate file

---
+++ src/database.py
@@ -0,0 +1,5 @@
+# Database configurations
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
--- src/main.py
+++ src/main.py
@@ -4,13 +4,6 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database configurations
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
 # SQLAlchemy models
 class Item(Base):
 	__tablename__ = "items"
