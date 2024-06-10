fd560ca54758c95319a3499bb25c4bff8ebc5ff7
vitalie stinca <vitalicstinca123@gmail.com>
2024-06-10 15:29:02 +0300

    fix: Minor fixes to make the overall soft work

--- src/database.py
+++ src/database.py
@@ -1,3 +1,7 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 # Database configurations
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
+from database import Base, engine
 import uvicorn
 from fastapi import FastAPI
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+
 
 # SQLAlchemy models
 class Item(Base):
