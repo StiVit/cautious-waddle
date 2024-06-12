665576a5ba7643251c7f59c7539d1de833fc130c
vitalie stinca <vitalicstinca123@gmail.com>
2024-06-12 17:45:51 +0300

    feat: Implement run.py

--- app/main.py
+++ app/main.py
@@ -1,53 +1,11 @@
-from database import Base, engine, SessionLocal
-import uvicorn
 from fastapi import FastAPI
-from src.models import Item
+from app.database import engine, Base
+from app.routers import items
 
 Base.metadata.create_all(bind=engine)
 # FastAPI app instance
 app = FastAPI()
 
 
-# CRUD operations
-# Create (Create)
-@app.post("/items/")
-async def create_item(name: str, description: str):
-	db = SessionLocal()
-	db_item = Item(name=name, description=description)
-	db.add(db_item)
-	db.commit()
-	db.refresh(db_item)
-	return db_item
-
-
-# Read (GET)
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-	db = SessionLocal()
-	item = db.query(Item).filter(Item.id == item_id).first()
-	return item
-
-
-# Update (PUT)
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, name: str, description: str):
-	db = SessionLocal()
-	db_item = db.query(Item).filter(Item.id == item_id).first()
-	db_item.name = name
-	db_item.description = description
-	db.commit()
-	return db_item
-
-
-# Delete (DELETE)
-@app.delete("/items/{item_id}")
-async def delete_item(item_id: int):
-	db = SessionLocal()
-	db_item = db.query(Item).filter(Item.id == item_id).first()
-	db.delete(db_item)
-	db.commit()
-	return {"message": "Item deleted successfully"}
-
-
-if __name__ == "__main__":
-	uvicorn.run(app)
+# Include routers
+app.include_router(items.router, prefix="/api", tags=["items"])
