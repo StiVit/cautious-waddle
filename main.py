6cd6ed2b952f8eaa4c12382da7cfdac1ff178edf
vitalie stinca <vitalicstinca123@gmail.com>
2024-07-23 17:41:49 +0300

    feat: Change the .env naming and its usage in the app

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from app.database import engine, Base
 from app.routers import endpoints
 from process_csv import run_script
 from app.utils.logger_config import setup_logger
+from app.utils.config import back_env
 
 Base.metadata.create_all(bind=engine)
 # FastAPI app instance
 app = FastAPI()
 
-app_logger = setup_logger('app_logger', logging.INFO)
+app_logger = setup_logger('app_logger', logging.INFO if back_env == "DEV" else logging.DEBUG)
 
 # Include routers
 app.include_router(endpoints.router, prefix="/api", tags=["items"])
